47b7ebffc56ddf57dc1d304a26bf651ff2e163a3
Hatem Ali <hatemali716@gmail.com>
2026-03-16 01:41:18 +0200

    code review

---
+++ src/Utils.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Utils {
+
+    public static String getItemInput(){
+        Scanner scanner = new Scanner(System.in);
+        String itemName=scanner.next();
+        return itemName.toLowerCase();
+    }
+    public static int getNumberInput(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
